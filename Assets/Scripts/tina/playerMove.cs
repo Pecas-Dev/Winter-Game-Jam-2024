b9ddb44b5693dad54cdeeb06e3ed7a1491e04303
thuytienhapriv <thuy-tien.ha.7351@student.uu.se>
2024-11-29 21:47:24 +0100

    idk man

---
+++ Assets/Scripts/tina/playerMove.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class playerMove : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x + 1, gameObject.transform.position .y, gameObject.transform.position.z);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x - 1, gameObject.transform.position.y, gameObject.transform.position.z);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y - 1, gameObject.transform.position.z);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 1, gameObject.transform.position.z);
+        }
+    }
+}
