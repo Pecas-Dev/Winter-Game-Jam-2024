b5ff50bb3665389af3522d7d8807c5386339ffa0
thuytienhapriv <thuy-tien.ha.7351@student.uu.se>
2024-11-30 11:12:47 +0100

    objective system fixes to textugui

--- Assets/Scripts/tina/objectiveSystem.cs
+++ Assets/Scripts/tina/objectiveSystem.cs
@@ -1,23 +1,12 @@
 using System.Collections.Generic;
 using NUnit.Framework;
+using TMPro;
 using Unity.Multiplayer.Center.Common;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class objectiveSystem : MonoBehaviour
 {
-    public class obj
-    {
-        public string name;
-        public bool conditionMet;
-    }
-
-    public List<obj> objectivesList = new List<obj>();
-    public List<obj> gameObjectives = new List<obj>();
-
-    // which objective, is it fulfilled
-    //public Dictionary<int, bool> objectives = new Dictionary<int, bool>();
-
     // 1 light the tree on fire
     // 2 steal a cat
     // 3 destroy 3 vases
@@ -29,9 +18,20 @@ public class objectiveSystem : MonoBehaviour
     // 9 clog the toilet (toilet paper disappears)
     // 10 chug their glogg
 
+    public class obj
+    {
+        public string name;
+        public bool conditionMet;
+    }
+
+    public List<obj> objectivesList = new List<obj>(); // in total
+    public List<obj> gameObjectives = new List<obj>(); // for this game
+
     public int vaseCount;
     public int carpetCount;
     public int presentCount;
+
+    [SerializeField] private TextMeshProUGUI[] objectiveText = new TextMeshProUGUI[3];
 
     void Start()
     {
@@ -41,10 +41,10 @@ public class objectiveSystem : MonoBehaviour
 
         objectivesList.Add(new obj { name = "Set Christmas Tree on fire", conditionMet = false });
         objectivesList.Add(new obj { name = "Steal a cat", conditionMet = false });
-        objectivesList.Add(new obj { name = "Destroy 3 vases (" + vaseCount.ToString() + "/3)", conditionMet = false });
+        objectivesList.Add(new obj { name = "Destroy 3 vases", conditionMet = false });
         objectivesList.Add(new obj { name = "Rip the couch", conditionMet = false });
-        objectivesList.Add(new obj { name = "Stain 2 carpets (" + carpetCount.ToString() + "/2)", conditionMet = false });
-        objectivesList.Add(new obj { name = "Steal 4 presents (" + presentCount.ToString() + "/4)", conditionMet = false });
+        objectivesList.Add(new obj { name = "Stain 2 carpets", conditionMet = false });
+        objectivesList.Add(new obj { name = "Steal 4 presents", conditionMet = false });
         objectivesList.Add(new obj { name = "Spill milk and eat the cookies", conditionMet = false });
         objectivesList.Add(new obj { name = "Unplug the fridge", conditionMet = false });
         objectivesList.Add(new obj { name = "Clog the toilet", conditionMet = false });
@@ -53,9 +53,26 @@ public class objectiveSystem : MonoBehaviour
         for (int i = 0; i < 3; i++)
         {
             int random = Random.Range(0, objectivesList.Count-1);
+
             gameObjectives.Add(objectivesList[random]);
+            string text = objectivesList[random].name;
+            
+            // expand text if necessary
+            if (text == "Destroy 3 vases")
+            {
+                text += " (" + vaseCount.ToString() + "/3)";
+            } else if (text == "Stain 2 carpets")
+            {
+                text += " (" + carpetCount.ToString() + "/2)";
+            } else if (text == "Steal 4 presents")
+            {
+                text += " (" + presentCount.ToString() + "/4)";
+            }
+
+            objectiveText[i].text = text;
             objectivesList.Remove(objectivesList[random]);
         }
+
 
         for (int i = 0; i < 3; i++)
         {
@@ -63,7 +80,86 @@ public class objectiveSystem : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
+    // destroy a vase, check if all vases have been destroyed & change conditionMet
+    public void DestroyVases()
+    {
+        vaseCount++;
+
+        if (vaseCount == 3) 
+        {
+            for (int i = 0; i < gameObjectives.Count; i++) 
+            { 
+                if (gameObjectives[i].name == "Destroy 3 vases")
+                {
+                    gameObjectives[i].conditionMet = true;
+                    return;
+                }
+            }
+        }
+    }
+
+    // stain a carpet, check if all carpets have been stained & change conditionMet
+    public void StainCarpets()
+    {
+        carpetCount++;
+
+        if (carpetCount == 2)
+        {
+            for (int i = 0; i < gameObjectives.Count; i++)
+            {
+                if (gameObjectives[i].name == "Stain 2 carpets")
+                {
+                    gameObjectives[i].conditionMet = true;
+                    return;
+                }
+            }
+        }
+    }
+
+    // steal a present, check if all presents have been stolen & change conditionMet
+    public void StealPresents()
+    {
+        presentCount++;
+
+        if (presentCount == 4)
+        {
+            for (int i = 0; i < gameObjectives.Count; i++)
+            {
+                if (gameObjectives[i].name == "Steal 4 presents")
+                {
+                    gameObjectives[i].conditionMet = true;
+                    return;
+                }
+            }
+        }
+    }
+
+    // change conditionMet based on objective name
+    public void OtherConditionMet(string objectiveName)
+    {
+        for (int i = 0; i < gameObjectives.Count; i++)
+        {
+            if (gameObjectives[i].name == objectiveName)
+            {
+                gameObjectives[i].conditionMet = true;
+                return;
+            }
+        }
+    }
+
+    // check if all 3 game objectives have their conditions met
+    private bool CheckIfAllConditionsAreMet()
+    {
+        for (int i = 0; i < gameObjectives.Count; i++)
+        {
+            if (gameObjectives[i].conditionMet == false)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    
     void Update()
     {
         
